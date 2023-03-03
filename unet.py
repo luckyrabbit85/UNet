243f6dde8b57f86e87530568891357e04064b82a
Aman Roland <amanroland@gmail.com>
2023-03-03 10:11:38 +0530

    added comments

--- unet.py
+++ unet.py
@@ -1,84 +1,263 @@
 import torch
-import torch.nn as nn
+from torch import nn
+import torchvision.transforms.functional as functional
 
 
-def double_conv(input_channels, output_channels):
-    conv = nn.Sequential(
-        nn.Conv2d(input_channels, output_channels, kernel_size=3),
-        nn.ReLU(inplace=True),
-        nn.Conv2d(output_channels, output_channels, kernel_size=3),
-        nn.ReLU(inplace=True),
-    )
-    return conv
+class DoubleConvolution(nn.Module):
+    """
+    A module that performs a double convolution on a 2D input tensor.
 
+    Args:
+        in_channels (int): Number of input channels.
+        out_channels (int): Number of output channels.
 
-def crop_img(tensor, target_tensor):
-    tensor_size = tensor.shape[2]
-    target_size = target_tensor.shape[2]
-    delta = tensor_size - target_size
-    delta = delta//2
-    return tensor[:, :, delta:tensor_size-delta, delta:tensor_size-delta]
+    Returns:
+        torch.Tensor: The output tensor of shape (batch_size, out_channels, H, W).
 
+    """
 
-class UNet(nn.Module):
-    def __init__(self) -> None:
+    def __init__(self, in_channels: int, out_channels: int):
+        """
+        Initializes the DoubleConvolution module.
+
+        Args:
+            in_channels (int): Number of input channels.
+            out_channels (int): Number of output channels.
+        """
         super().__init__()
 
-        self.max_pool_2x2 = nn.MaxPool2d(kernel_size=2, stride=2)
+        # First convolution layer with 3x3 kernel
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3)
 
-        self.down_conv_1 = double_conv(1, 64)
-        self.down_conv_2 = double_conv(64, 128)
-        self.down_conv_3 = double_conv(128, 256)
-        self.down_conv_4 = double_conv(256, 512)
-        self.down_conv_5 = double_conv(512, 1024)
+        # ReLU activation after first convolution
+        self.act1 = nn.ReLU()
 
-        self.up_trans_1 = nn.ConvTranspose2d(
-            1024, 512, kernel_size=2, stride=2)
-        self.up_trans_2 = nn.ConvTranspose2d(
-            512, 256, kernel_size=2, stride=2)
-        self.up_trans_3 = nn.ConvTranspose2d(
-            256, 128, kernel_size=2, stride=2)
-        self.up_trans_4 = nn.ConvTranspose2d(
-            128, 64, kernel_size=2, stride=2)
+        # Second convolution layer with 3x3 kernel
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3)
 
-        self.up_conv_1 = double_conv(1024, 512)
-        self.up_conv_2 = double_conv(512, 256)
-        self.up_conv_3 = double_conv(256, 128)
-        self.up_conv_4 = double_conv(128, 64)
+        # ReLU activation after second convolution
+        self.act2 = nn.ReLU()
 
-        self.out = nn.Conv2d(64, 2, kernel_size=1)
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """
+        Performs a forward pass of the DoubleConvolution module.
 
-    def forward(self, image):
-        # encoder
-        x1 = self.down_conv_1(image)
-        x2 = self.max_pool_2x2(x1)
-        x3 = self.down_conv_2(x2)
-        x4 = self.max_pool_2x2(x3)
-        x5 = self.down_conv_3(x4)
-        x6 = self.max_pool_2x2(x5)
-        x7 = self.down_conv_4(x6)
-        x8 = self.max_pool_2x2(x7)
-        x9 = self.down_conv_5(x8)
+        Args:
+            x (torch.Tensor): The input tensor of shape (batch_size, in_channels, H, W).
 
-        # decoder
-        x = self.up_trans_1(x9)
-        y = crop_img(x7, x)
-        x = self.up_conv_1(torch.cat([x, y], 1))
-        x = self.up_trans_2(x)
-        y = crop_img(x5, x)
-        x = self.up_conv_2(torch.cat([x, y], 1))
-        x = self.up_trans_3(x)
-        y = crop_img(x3, x)
-        x = self.up_conv_3(torch.cat([x, y], 1))
-        x = self.up_trans_4(x)
-        y = crop_img(x1, x)
-        x = self.up_conv_4(torch.cat([x, y], 1))
-        x = self.out(x)
+        Returns:
+            torch.Tensor: The output tensor of shape (batch_size, out_channels, H, W).
+        """
+        # First convolution followed by ReLU activation
+        x = self.conv1(x)
+        x = self.act1(x)
+
+        # Second convolution followed by ReLU activation
+        x = self.conv2(x)
+        x = self.act2(x)
+
+        # Return the output tensor
         return x
 
 
-if __name__ == '__main__':
+class DownSample(nn.Module):
+    """
+    A module that performs max pooling on a 2D input tensor.
+
+    Returns:
+        torch.Tensor: The output tensor of shape (batch_size, in_channels, H/2, W/2).
+    """
+
+    def __init__(self) -> None:
+        """
+        Initializes the DownSample module.
+        """
+        super().__init__()
+
+        # Max pooling layer with 2x2 kernel and stride 2
+        self.maxpool = nn.MaxPool2d(kernel_size=2, stride=2)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """
+        Performs a forward pass of the DownSample module.
+
+        Args:
+            x (torch.Tensor): The input tensor of shape (batch_size, in_channels, H, W).
+
+        Returns:
+            torch.Tensor: The output tensor of shape (batch_size, in_channels, H/2, W/2).
+        """
+        # Apply max pooling and return the output tensor
+        return self.maxpool(x)
+
+
+class UpSample(nn.Module):
+    """
+    A module that performs transposed convolution on a 2D input tensor.
+
+    Args:
+        in_channels (int): Number of input channels.
+        out_channels (int): Number of output channels.
+
+    Returns:
+        torch.Tensor: The output tensor of shape (batch_size, out_channels, H*2, W*2).
+    """
+
+    def __init__(self, in_channels: int, out_channels: int):
+        """
+        Initializes the UpSample module.
+
+        Args:
+            in_channels (int): Number of input channels.
+            out_channels (int): Number of output channels.
+        """
+        super().__init__()
+
+        # Transposed convolution layer with 2x2 kernel and stride 2
+        self.conv_trans = nn.ConvTranspose2d(
+            in_channels, out_channels, kernel_size=2, stride=2
+        )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """
+        Performs a forward pass of the UpSample module.
+
+        Args:
+            x (torch.Tensor): The input tensor of shape (batch_size, in_channels, H, W).
+
+        Returns:
+            torch.Tensor: The output tensor of shape (batch_size, out_channels, H*2, W*2).
+        """
+        # Apply transposed convolution and return the output tensor
+        return self.conv_trans(x)
+
+
+class CropAndConcat(nn.Module):
+    """
+    A module that crops and concatenates two 2D input tensors along the channel axis.
+
+    Args:
+        x (torch.Tensor): The first input tensor.
+        contracting_x (torch.Tensor): The second input tensor to be cropped and concatenated.
+
+    Returns:
+        torch.Tensor: The concatenated tensor of shape (batch_size, in_channels_x + in_channels_contracting_x, H, W).
+    """
+
+    def forward(self, x: torch.Tensor, contracting_x: torch.Tensor) -> torch.Tensor:
+        """
+        Performs a forward pass of the CropAndConcat module.
+
+        Args:
+            x (torch.Tensor): The first input tensor.
+            contracting_x (torch.Tensor): The second input tensor to be cropped and concatenated.
+
+        Returns:
+            torch.Tensor: The concatenated tensor of shape (batch_size, in_channels_x + in_channels_contracting_x, H, W).
+        """
+        # Crop the second tensor to have the same size as the first tensor
+        contracting_x = functional.center_crop(contracting_x, [x.shape[2], x.shape[3]])
+
+        # Concatenate the two tensors along the channel axis and return the output tensor
+        x = torch.cat([x, contracting_x], dim=1)
+        return x
+
+
+class UNet(nn.Module):
+    """
+    A U-Net model for semantic segmentation of images.
+
+    The architecture of the U-Net consists of a contracting path (downsampling path) and an expanding path (upsampling
+    path). The contracting path consists of several blocks, each consisting of two 2D convolutions with ReLU activation
+    and followed by 2D max pooling. The expanding path consists of several blocks, each consisting of an upsampling
+    layer, concatenation with the corresponding block of the contracting path, followed by two 2D convolutions with
+    ReLU activation. The final output of the network is a 2D convolutional layer with two output channels.
+
+    Args:
+        None
+
+    Returns:
+        torch.Tensor: The output tensor of shape (batch_size, 2, H, W).
+    """
+
+    def __init__(self) -> None:
+        """
+        Initializes the UNet module.
+
+        Args:
+            None
+
+        Returns:
+            None
+        """
+        super().__init__()
+        # Create the contracting path
+        self.down_conv = nn.ModuleList(
+            [
+                DoubleConvolution(in_c, out_c)
+                for in_c, out_c in [(1, 64), (64, 128), (128, 256), (256, 512)]
+            ]
+        )
+        self.down_sample = nn.ModuleList([DownSample() for _ in range(4)])
+
+        # Create the bottleneck layer
+        self.bottleneck_conv = DoubleConvolution(512, 1024)
+
+        # Create the expanding path
+        self.up_sample = nn.ModuleList(
+            [
+                UpSample(in_c, out_c)
+                for in_c, out_c in [(1024, 512), (512, 256), (256, 128), (128, 64)]
+            ]
+        )
+        self.up_conv = nn.ModuleList(
+            [
+                DoubleConvolution(in_c, out_c)
+                for in_c, out_c in [(1024, 512), (512, 256), (256, 128), (128, 64)]
+            ]
+        )
+        self.concat = nn.ModuleList([CropAndConcat() for _ in range(4)])
+
+        # Create the final output layer
+        self.final_conv = nn.Conv2d(64, 2, kernel_size=1)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """
+        Performs a forward pass of the UNet module.
+
+        Args:
+            x (torch.Tensor): The input tensor of shape (batch_size, 1, H, W).
+
+        Returns:
+            torch.Tensor: The output tensor of shape (batch_size, 2, H, W).
+        """
+        features = []
+        # Pass the input through the contracting path
+        for i in range(len(self.down_conv)):
+            x = self.down_conv[i](x)
+            features.append(x)
+            x = self.down_sample[i](x)
+
+        # Pass the input through the bottleneck layer
+        x = self.bottleneck_conv(x)
+
+        # Pass the input through the expanding path
+        for i in range(len(self.up_conv)):
+            x = self.up_sample[i](x)
+            x = self.concat[i](x, features.pop())
+            x = self.up_conv[i](x)
+
+        # Pass the output through the final convolutional layer
+        x = self.final_conv(x)
+        return x
+
+
+if __name__ == "__main__":
+    # Create a random tensor of size (1, 1, 572, 572)
     image = torch.rand((1, 1, 572, 572))
+    # Initialize an instance of the UNet model
     model = UNet()
+    # Pass the input tensor through the model
     result = model(image)
+    # Print the size of the output tensor
     print(result.size())
